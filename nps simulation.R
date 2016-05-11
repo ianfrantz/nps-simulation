df2283e801aa4518d7c8bfa2bc2505947488a3ba
ianfrantz <ianfrantz@gmail.com>
2016-05-10 20:36:23 -0700

    Core Read Me, NPS Script and R Project bundle

---
+++ nps simulation.R
@@ -0,0 +1,19 @@
+##NPS Simulation
+
+outcomes = c(1:3)
+nps <- c()    
+x <- c()
+for (i in 1:5000) 
+  {
+  npssimulation <- sample(outcomes, size = 100, replace = TRUE, prob = c(.60,.20,.20))
+    detractor <- subset(npssimulation, npssimulation == 1)
+    passive <- subset (npssimulation, npssimulation == 2)
+    promoter <- subset(npssimulation, npssimulation == 3)
+    totalresp <- sum(length(promoter),length(detractor), length(passive))
+    x <- (length(promoter) - length(detractor))
+    x <- (x / length(totalresp))
+  nps <- append(nps, x)
+}
+mean(nps)
+npsdistribution <- density(nps)
+plot (npsdistribution)
